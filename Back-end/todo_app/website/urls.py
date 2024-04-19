5d7c8431b8a9d54a93cb30650b898c6a5d8948cb
xudayfi <xudayfiibraahim8@gmail.com>
2024-04-19 21:43:45 +0300

    ended the serializers and testing restframe work also the backend side is complete

--- Back-end/todo_app/website/urls.py
+++ Back-end/todo_app/website/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'tasks', views.TodoViews, 'task')
+
 
 urlpatterns = [
-    path('', views.test, name="test")
+    path('', views.test, name="test"),
+    path('api/', include(router.urls))
 ]
--- Back-end/todo_app/website/views.py
+++ Back-end/todo_app/website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from . models import Todo
+from . serializers import TodoSerializers
+from rest_framework import viewsets
 
 def test(request):
     return HttpResponse('we good to go')
+
+class TodoViews(viewsets.ModelViewSet):
+    serializer_class = TodoSerializers
+    queryset = Todo.objects.all()
