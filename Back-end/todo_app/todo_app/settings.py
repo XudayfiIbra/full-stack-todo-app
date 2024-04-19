2c434779d7a31a2992133bb124bcf05c85ffc4f6
xudayfi <xudayfiibraahim8@gmail.com>
2024-04-19 21:14:21 +0300

    project setup is done also the models is done

--- Back-end/todo_app/todo_app/settings.py
+++ Back-end/todo_app/todo_app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# headers for react js Localhost:3000
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
+ 
--- Back-end/todo_app/website/models.py
+++ Back-end/todo_app/website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    is_completed= models.BooleanField(default=False) 
+    
+    def __str__(self):
+        return self.title
+    
